8a263a12f2ffe69cb38ebba22da27950fcc72fdf
kmf <kmf@kmf.co>
2020-10-26 16:25:15 +0200

    added dependancy to the Docker Cookbook

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures factorio_docker_cookbook'
 version '0.1.0'
 chef_version '>= 15.0'
+depends 'docker'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
